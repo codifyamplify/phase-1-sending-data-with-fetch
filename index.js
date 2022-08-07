adeeb0d1ecd900440070a66de450c35ae0a26623
David Whitney <codifyamplify@gmail.com>
2022-08-06 21:54:33 -0400

    commit tree d084edd274df7cff83865457eb2c3ee3d5efa8c4

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+            name,
+            email
+        })
+    })
+    .then(function (response) {
+        // console.log(response)
+        return response.json()
+    })
+    .then(function (object) {
+        // object.querySelector("object.id").appendChild()
+        // console.log(object.id)
+        document.body.innerHTML = object.id
+    })
+    .catch(function (error) {
+        document.body.innerHTML = error.message
+    })
+}
+
+// submitData('john', 'john@gmail.com')
